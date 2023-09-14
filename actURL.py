bab14685a2f74a6a653b12b7d81fa4e7a1ad709c
brunky93 <74769651+brunky93@users.noreply.github.com>
2023-09-14 12:41:03 +0000

    Script externo

--- actURL.py
+++ actURL.py
@@ -3,23 +3,21 @@ import subprocess
 subprocess.check_call(['pip', 'install', 'beautifulsoup4'])
 # Llamada al comando pip para instalar la librería pyperclip
 subprocess.check_call(['pip', 'install', 'pyperclip'])
+# Llamada al comando pip para instalar la librería argparse
+subprocess.check_call(['pip', 'install', 'argparse'])
 
 import os
+import argparse
 from bs4 import BeautifulSoup
 
 # Directorio donde se encuentran los archivos HTML
 # Directorio actual donde se encuentra el script
 directorio_script = os.path.dirname(os.path.abspath(__file__))
 
-# Solicitar la URL nueva al usuario
-url_nueva = input("Por favor, ingresa la nueva URL y presiona Enter: ")
-
-# Función para actualizar los enlaces en un archivo HTML
 def actualizar_enlace_en_html(html_file, url_nueva):
     with open(html_file, 'r') as f:
         soup = BeautifulSoup(f, 'html.parser')
 
-    # Encuentra todos los enlaces que necesitas actualizar
     enlaces = soup.find_all('a', href=lambda href: href and 'couponCode' in href)
 
     for enlace in enlaces:
@@ -28,11 +26,20 @@ def actualizar_enlace_en_html(html_file, url_nueva):
     with open(html_file, 'w') as f:
         f.write(str(soup))
 
-# Recorre los archivos HTML en el directorio y actualiza los enlaces
-for root, dirs, files in os.walk(directorio_script):
-    for archivo in files:
-        if archivo.endswith('.html'):
-            archivo_html = os.path.join(root, archivo)
-            actualizar_enlace_en_html(archivo_html, url_nueva)
+def main():
+    parser = argparse.ArgumentParser(description='Actualizar enlaces en archivos HTML con una URL nueva.')
+    parser.add_argument('url_nueva', type=str, help='La nueva URL a utilizar para actualizar los enlaces.')
 
-print("Enlaces actualizados en todos los archivos HTML con la URL nueva ingresada.")
+    args = parser.parse_args()
+    url_nueva = args.url_nueva
+
+    for root, dirs, files in os.walk(directorio_script):
+        for archivo in files:
+            if archivo.endswith('.html'):
+                archivo_html = os.path.join(root, archivo)
+                actualizar_enlace_en_html(archivo_html, url_nueva)
+
+    print("Enlaces actualizados en todos los archivos HTML con la URL nueva proporcionada.")
+
+if __name__ == '__main__':
+    main()
